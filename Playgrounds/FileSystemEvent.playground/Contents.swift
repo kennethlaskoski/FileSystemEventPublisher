3433cb983853e2e62d3678841e5a9498dce46976
Kenneth Laskoski <kennethlaskoski@gmail.com>
2021-07-22 21:59:59 -0300

    Added playground

---
+++ Playgrounds/FileSystemEvent.playground/Contents.swift
@@ -0,0 +1,25 @@
+import System
+import Foundation
+import FileSystemEventPublisher
+
+let id = UUID()
+let tmpURL = FileManager.default.temporaryDirectory
+let url = URL(fileURLWithPath: "\(id)", relativeTo: tmpURL)
+let tmp = try! FileDescriptor.open(tmpURL.path, .readOnly, options: .eventOnly)
+
+var received = DispatchSource.FileSystemEvent()
+
+print(received)     // prints "FileSystemEvent(rawValue: 0)"
+
+let cancellable = DispatchSource.publish(at: tmp)
+  .sink { event in
+    received = event
+  }
+
+try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: false, attributes: nil)
+
+sleep(1)
+
+print(received)     // prints "FileSystemEvent(rawValue: 18)"
+
+cancellable.cancel()
