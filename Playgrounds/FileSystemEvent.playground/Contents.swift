bb37313df809d989e9f43d93e1c4edaa4ee66e0f
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-06-20 14:21:04 -0300

    Update example playground

--- Playgrounds/FileSystemEvent.playground/Contents.swift
+++ Playgrounds/FileSystemEvent.playground/Contents.swift
@@ -2,24 +2,33 @@ import System
 import Foundation
 import FileSystemEventPublisher
 
-let id = UUID()
-let tmpURL = FileManager.default.temporaryDirectory
-let url = URL(fileURLWithPath: "\(id)", relativeTo: tmpURL)
-let tmp = try! FileDescriptor.open(tmpURL.path, .readOnly, options: .eventOnly)
-
+// First we declare a varialble to store received events
 var received = FileSystemEventPublisher.Event()
-
+// Initially, we don't have any event
+assert(received.isEmpty)
 print(received)     // prints "FileSystemEvent(rawValue: 0)"
 
-let cancellable = FileSystemEventPublisher.monitor(tmp, for: .all)
-  .sink { event in
-    received = event
-  }
+// Then we get the system's tmp folder url
+let tmpURL = FileManager.default.temporaryDirectory
+// And a file descriptor referencing it
+let tmpFileDescriptor = try! FileDescriptor.open(tmpURL.path, .readOnly, options: .eventOnly)
 
+// Now we can get a publisher to monitor the tmp folder
+let publisher = FileSystemEventPublisher.monitor(tmpFileDescriptor, for: .all)
+
+// When events arrive,
+// we update our variable
+let cancellable = publisher.sink { received = $0 }
+
+// We create a new folder
+let id = UUID()
+let url = URL(fileURLWithPath: "\(id)", relativeTo: tmpURL)
 try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: false, attributes: nil)
 
-sleep(1)
-
+// And this is reflected in our variable
+assert(received.contains(.write))
 print(received)     // prints "FileSystemEvent(rawValue: 18)"
 
+// Finally, we cancel the subscription
+// which will close the file descriptor
 cancellable.cancel()
